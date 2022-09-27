1042c6a0cf092bfd6f7bbc9bac24cdadd34815fe
ncut-ai <1053940@qq.com>
2022-09-27 12:06:09 +0800

    sumo

--- Sumo+Python示例/README.md
+++
@@ -1,2 +0,0 @@
-# pycharm-sumo
-用SUMO简单的创建一个路口，并用pycharm调用并仿真
--- 简单的例子/生产rou文件jtrrouter.bat
+++
@@ -1,5 +0,0 @@
-jtrrouter -c test.jtrrcfg.xml --accept-all-destinations
-
-::jtrrouter --flow-files=test.flows.xml --turn-ratio-files=test.turns.xml --net-file=test.net.xml --output-file=test.rou.xml --begin 1 --end 1000
-  
-pause
